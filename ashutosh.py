066ed804d1e33811bf1adb359d9b82cc1a250ac2
ashutosh1098 <113101826+ashutosh1098@users.noreply.github.com>
2022-12-07 12:17:52 +0530

    Add files via upload

---
+++ ashutosh.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec  7 12:09:45 2022
+
+@author: Ashutosh Verma
+"""
+
+def even_check(n):
+    if n%2 ==0:
+        print("number is even")
+    else :
+        print("number is odd")
+        
+def factor(n):
+    temp =[]
+    for i in range(1,n):
+        if n%i==0:
+            temp.append(i)
+    return temp
+
