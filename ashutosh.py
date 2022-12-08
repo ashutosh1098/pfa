6b0b3f787e46b07c00dc618648d772764c014c7f
ashutosh1098 <113101826+ashutosh1098@users.noreply.github.com>
2022-12-08 12:38:29 +0530

    Update ashutosh.py

--- ashutosh.py
+++ ashutosh.py
@@ -16,7 +16,7 @@ def factor(n):
     for i in range(1,n):
         if n%i==0:
             temp.append(i)
-        return temp
-    print(*temp)
+    for i range (len(temp))
+        print(temp[i])
 
 
